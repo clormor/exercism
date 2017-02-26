5ce635f2fd1833902b371a4ace34b25fded57a41
Chris Lormor <clormor@palantir.com>
2017-02-26 16:45:01 +0000

    Completing phone number JS exercise

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,17 +1,29 @@
 
 var PhoneNumber = function(phoneNumber) {
     this.phoneNumber = ""
-    this.length = 0
     for (var i = 0; i < phoneNumber.length; i++) {
         if (phoneNumber[i].match(/[0-9]/)) {
             this.phoneNumber += phoneNumber[i]
-            this.length++
         }
+    }
+    if (this.phoneNumber.length == 11 && this.phoneNumber[0] == 1) {
+        this.phoneNumber = this.phoneNumber.substring(1,11)
+    }
+    if (this.phoneNumber.length != 10) {
+        this.phoneNumber = '0000000000'
     }
 };
 
 PhoneNumber.prototype.number = function() {
     return this.phoneNumber
+};
+
+PhoneNumber.prototype.areaCode = function() {
+    return this.phoneNumber.substring(0,3)
+};
+
+PhoneNumber.prototype.toString = function() {
+    return "(" + this.areaCode() + ") " + this.phoneNumber.substring(3,6) + "-" + this.phoneNumber.substring(6,10)
 };
 
 module.exports = PhoneNumber;
