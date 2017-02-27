f360a6eacced43748d19bd842af1f49add2d7e93
Chris Lormor <clormor@palantir.com>
2017-02-27 22:01:06 +0000

    only declare character array once

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,19 +1,19 @@
 
-var usedNames = {}
+usedNames = {}
+chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var Robot = function() {
-    this.chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     this.reset()
 }
 
 Robot.prototype.reset = function() {
     do {
         this.name =
-                this.chars[Math.floor(Math.random() * 26)]
-                + this.chars[Math.floor(Math.random() * 26)]
-                + Math.floor(Math.random() * 10)
-                + Math.floor(Math.random() * 10)
-                + Math.floor(Math.random() * 10)
+                chars[Math.floor(Math.random() * 26)] +
+                chars[Math.floor(Math.random() * 26)] +
+                Math.floor(Math.random() * 10) +
+                Math.floor(Math.random() * 10) +
+                Math.floor(Math.random() * 10)
     } while(usedNames[this.name] == true)
 
     usedNames[this.name] = true
