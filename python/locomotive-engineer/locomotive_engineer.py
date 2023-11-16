ebc86e7bfb88cbe8b9044137576a8230f97a4b11
Chris Lormor <clormor@palantir.com>
2023-11-16 20:41:21 +0000

    yay

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,14 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons(*args):
+def get_list_of_wagons(*wagons):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    return list(args)
+    *wagon_list, = wagons
+    return wagon_list
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
@@ -21,14 +22,15 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     *result, = locomotive, *missing_wagons, *remaining_wagons, end_wagon_1, end_wagon_2
     return result
 
-def add_missing_stops():
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    *stop_list, = stops.values()
+    return {**route , **{"stops": stop_list}}
 
 
 def extend_route_information(route, more_route_information):
@@ -38,7 +40,7 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return {**route, **more_route_information}
 
 
 def fix_wagon_depot(wagons_rows):
@@ -47,4 +49,9 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    [[row1_a, row1_b, row1_c], [row2_a, row2_b, row2_c], [row3_a, row3_b, row3_c]] = wagons_rows
+    return [
+        [row1_a, row2_a, row3_a],
+        [row1_b, row2_b, row3_b],
+        [row1_c, row2_c, row3_c] 
+    ]
